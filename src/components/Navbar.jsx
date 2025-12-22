5de88379d8cd5a350a159b8ea73b1800af117b02
Ankit <hazraankit668@gmail.com>
2025-12-23 01:05:27 +0530

    ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../auth/AuthContext";
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 export default function Navbar() {
@@ -11,26 +11,26 @@ export default function Navbar() {
 
   return (
     <nav className="fixed top-0 w-full z-50 bg-black/40 backdrop-blur-md border-b border-white/10">
-      <div className="max-w-7xl mx-auto px-8 py-4 flex justify-between items-center">
-        
-        <Link to="/" className="flex items-center gap-3">
-            <img
-              src="/Icon.svg"
-              alt="Mentivra Logo"
-              className="h-8 w-8 object-contain"
-            />
-            <span className="text-white font-semibold tracking-wide text-lg">
-              Mentivra
-            </span>
-          </Link>
+      <div className="max-w-7xl mx-auto px-4 sm:px-8 py-4 flex justify-between items-center ">
 
-        <div className="flex gap-6 text-sm text-gray-300">
+        <Link to="/" className="flex items-center gap-3">
+          <img
+            src="/Icon.svg"
+            alt="Mentivra Logo"
+            className="h-8 w-8 object-contain"
+          />
+          <span className="text-white font-semibold tracking-wide text-lg">
+            Mentivra
+          </span>
+        </Link>
+
+        <div className="flex gap-10 text-sm text-gray-300 ml-auto">
           {user ? (
             <>
               <Link to="/dashboard" className="hover:text-white hover:scale-105 transform transition duration-300">
                 Dashboard
               </Link>
-              <button onClick={()=>{
+              <button onClick={() => {
                 logout();
                 navigate("/login");
               }} className="hover:text-white hover:scale-105 transform transition duration-300">
@@ -44,9 +44,9 @@ export default function Navbar() {
               </Link>
               <Link
                 to="/signup"
-                className="px-4 py-2 rounded-md bg-indigo-600 hover:bg-indigo-400 text-white hover:scale-105 transform transition duration-300 hover:scale-105 transform transition duration-300"
+                className="px-4 py-2 rounded-md bg-indigo-600 hover:bg-indigo-400 text-white hover:scale-105 transform transition duration-300"
               >
-                Get Started
+                Sign Up
               </Link>
             </>
           )}
