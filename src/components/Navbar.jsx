784f50d43487f28242daf822a33953f7aa3461f4
Ankit <hazraankit668@gmail.com>
2025-12-23 00:53:29 +0530

    ui ux fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,34 +12,39 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 w-full z-50 bg-black/40 backdrop-blur-md border-b border-white/10">
       <div className="max-w-7xl mx-auto px-8 py-4 flex justify-between items-center">
-        <Link
-          to="/"
-          className="text-xl font-semibold tracking-wide text-white"
-        >
-          Mentivra
-        </Link>
+        
+        <Link to="/" className="flex items-center gap-3">
+            <img
+              src="/Icon.svg"
+              alt="Mentivra Logo"
+              className="h-8 w-8 object-contain"
+            />
+            <span className="text-white font-semibold tracking-wide text-lg">
+              Mentivra
+            </span>
+          </Link>
 
         <div className="flex gap-6 text-sm text-gray-300">
           {user ? (
             <>
-              <Link to="/dashboard" className="hover:text-white">
+              <Link to="/dashboard" className="hover:text-white hover:scale-105 transform transition duration-300">
                 Dashboard
               </Link>
               <button onClick={()=>{
                 logout();
                 navigate("/login");
-              }} className="hover:text-white">
+              }} className="hover:text-white hover:scale-105 transform transition duration-300">
                 Logout
               </button>
             </>
           ) : (
             <>
-              <Link to="/login" className="hover:text-white mt-1 text-xl ">
+              <Link to="/login" className="hover:text-white mt-1 text-l hover:scale-120 transform transition duration-300">
                 Login
               </Link>
               <Link
                 to="/signup"
-                className="px-4 py-2 rounded-md bg-indigo-600 hover:bg-indigo-500 text-white"
+                className="px-4 py-2 rounded-md bg-indigo-600 hover:bg-indigo-400 text-white hover:scale-105 transform transition duration-300 hover:scale-105 transform transition duration-300"
               >
                 Get Started
               </Link>
