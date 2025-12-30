9a50e05d6a70b41ff92479226e8db96e4c38b972
Ankit <hazraankit668@gmail.com>
2025-12-30 21:54:33 +0530

    added og image.png

--- src/components/GlassCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function GlassCard({ children, className = "" }) {
-  return (
-    <div
-      className={`rounded-xl bg-white/70 backdrop-blur-glass shadow-glass border border-white/40 ${className}`}
-    >
-      {children}
-    </div>
-  );
-}
